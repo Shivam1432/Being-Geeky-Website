3ed63084dd3706518062fa317db3dceff5ce7138
Shivam <shivam1634.cse18@chitkara.edu.in>
2021-03-12 21:39:57 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import "./style.css";
+
+class Cq extends React.Component {
+	render() {
+		return (
+             <>
+             <html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></meta>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+<meta http-equiv="X-UA-Compatible" content="ie=edge"></meta>
+ <link rel="stylesheet" href="./src/style.css"></link>
+ <title>Being Geeky</title>
+ </head>
+<body cz-shortcut-listen="true">
+<div id="showcase">
+ <header> 
+ <nav class="cf">
+ <ul class="cf"><li class="hide-on-small"> 
+ <a href="#showcase">Being Geeky</a></li><li>
+ <a href="#tutorial">Tutorials</a></li><li> 
+ <a href="#video">Video</a></li><li> 
+ <a href="#coupon">Coupons</a></li></ul>
+ <a href="#geeky" id="openup">Being Geeky</a>
+ </nav> 
+ </header>
+ <div class="section-main container">
+ <h1 class="h1">Being Geeky</h1>
+ <h2 class="h1">Your own video and theortical study place.</h2>
+ <p  class="lead hide-on-small" id="h12"> Being Geeky is one of the best platform for the online learning. With the best study material.Dummy text is text that is used in the publishing industry or by web designers to occupy the space which will later be filled with 'real' content. This is required when, for example, the final text is not yet available. Dummy text is also known as 'fill text'. It is said that song composers of the past used dummy texts as lyrics when writing melodies in order to have a 'ready-made' text to sing with the melody. Dummy texts have been in use by typesetters since the 16th century.</p>
+ </div>
+ </div>
+ <section  id="tutorial" class="section">
+ <div class="container">
+ <h2 class="section-head">
+ <i class="fas fa-music"></i> Documentation</h2>
+ <h3>All genuine Documentation at one place</h3>
+ <p class="lead"> Being Geeky helps the viewer to find all the required documentation at one place so that they need not waste time to find offical documentation. The most well-known dummy text is the 'Lorem Ipsum', which is said to have originated in the 16th century. Lorem Ipsum is composed in a pseudo-Latin language which more or less corresponds to 'proper' Latin. It contains a series of real Latin words. This ancient dummy text is also incomprehensible, but it imitates the rhythm of most European languages in Latin script. The advantage of its Latin origin and the relative meaninglessness of Lorum Ipsum is that the text does not attract attention to itself or distract the viewers attention from the layout.</p> 
+ <a href="https://beinggeeky-hnip75cdkgvvk6vt.web.codequotient.com" class="btn btn-secondary mb">Start Your Trial Now</a>
+ <p class="text-light"> You can download Being Geeky from app store or playstore.</p> 
+ <img id="1" src="./public/ipad-app-mockup-03.png" alt="IMAGE1"></img>
+ </div> 
+ </section>
+ <section id="video" class="section bg-light">
+ <div class="container"><h3>The video lecture you shouldn't miss</h3>
+ <p class="lead"> With one of the best content available online.And one of the best instructors it makes Being Geeky one of the best available learing platform available.he spread of computers and layout programmes thus made dummy text better known. While in earlier times several lines of the Lorem Ipsum text were repeated in the creation of dummy texts, today the full text of Cicero's 'De finibus serves as the basis for many dummy text or Lorem Ipsum generators. Based on 'De finibus', these generators automatically create longer sections of the Lorem Ipsum text or various other filler texts.</p>
+ <a href="#READ" class="btn btn-secondary">Read More</a>
+ <img id="2" src="./public/lecture.jpg" alt="IMAGE2">
+ </img>
+ </div> </section> 
+ <section id="entertainment" class="section">
+ <div class="container">
+ <h3>Lectures available anywhere anytime.</h3>
+ <p class="lead"> You can watch the lecture anytime from anywhere and ask doubts.The phrasal sequence of the Lorem Ipsum text is now so widespread and commonplace that many DTP programmes can generate dummy text using the starting sequence "Lorem ipsum". Fortunately, the phrase 'Lorem Ipsum' is now recognized by electronic pre-press systems and, when found, an alarm can be raised. This avoids a publication going to print with overlooked dummy text.</p>
+ </div> </section>
+ <section id="coupon" class="section bg-light">
+ <div class="container"><div class="gift-cards">
+ <div> <img id="3" src="./public/coupon.jpg" alt="IMAGE3"></img>
+ </div><div>
+ <h2>Coupons</h2><p> You can apply different coupons to get extra discount on selected courses.Certain internet providers exploit the fact that fill text cannot be recognized by automatic search engines - meaningful information cannot be distinguished from meaningless: Target-generated dummy text mixed with a certain combination of search terms can lead to an increased frequency of visits by search machine users. As a consequence, advertising revenues, which rely on website 'hits', are increased.</p><p>Already have Coupon?</p><hr></hr>
+ <a href="#Redeem" class="text-secondary">
+ <i class="fas fa-chevron-right"></i> Redeem </a>
+ </div></div></div> </section>
+ <footer class="footer">
+ <div class="container">
+ <div class="footer-cols">
+ <ul>
+ <li>Address</li><li>
+ ZZZZ Workspace</li>
+ <li> Street 43,building 4A</li>
+ <li> Industrial Area</li>
+ <li> Aqwertyu </li>
+ <li> Mumbai</li>
+ </ul>
+ <ul><li>Clients</li><li> XYZ org</li>
+ <li> ABC org</li>
+ <li> IJK org</li>
+ <li> Qwerty Corp</li>
+ <li> ZAQ PVT</li>
+ </ul>
+ <ul>
+ <li>Stack</li>
+ <li> HTML5,CSS,JS</li>
+ <li> React</li>
+ <li> Angular</li>
+ <li> NodeJS</li>
+ <li> Django</li>
+ </ul>
+ <ul>
+ <li>About Being Geeky</li>
+ <li> About CEO</li>
+ <li> About Team</li>
+ <li> Location</li>
+ <li> Working Atmosphere</li>
+ <li> Contact Being Geeky</li>
+ </ul>
+ </div></div> 
+ </footer> 
+ </body>
+ </html>
+             </>
+        )
+	}
+}
+
+ReactDOM.render(
+	<Cq />,
+	document.getElementById('root')
+);
